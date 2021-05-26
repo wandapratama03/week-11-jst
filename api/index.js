db90858e4d4876eec08920394effcb73abcd8cb2
Wanda Adi Pratama <p.wandaadi@gmail.com>
2021-05-26 22:27:21 +0700

    update index.js

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click /menu to main menu`
+        click /predict`
     );   
 });
 
@@ -27,6 +27,41 @@ bot.onText(/\/menu/, (msg) => {
         `this is your main menu`
     );   
 });
+
+state = 0
+bot.onText(/\/predict/, (msg) => { 
+    bot.sendMessage(
+        msg.chat.id,
+        `masukan nilai i|v contohnya 4|3`
+    );   
+    state = 1
+});
+
+bot.on('message', (msg) => {
+    if(state == 1){
+        s = msg.text.split("|");
+        i = s[0]
+        v = s[1]
+        model.predict(
+            [
+                parseFloat(s[0]), // string to float
+                parseFloat(s[1])
+            ]
+        ).then((jres)=>{
+            console.log(jres);
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai v yang diprediksi adalah s{jres[0]} volt`
+            );   
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai p yang diprediksi adalah s{jres[1]} watt`
+            );   
+        })
+    }else{
+        state = 0
+    }
+})
 
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
