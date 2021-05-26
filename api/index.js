508c284fc7d5638f168143fcb3f56b4859d0b479
wandapratama03 <84654036+wandapratama03@users.noreply.github.com>
2021-05-26 22:53:26 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -51,11 +51,11 @@ bot.on('message', (msg) => {
             console.log(jres);
             bot.sendMessage(
                 msg.chat.id,
-                `nilai v yang diprediksi adalah s.{jres[0]} volt`
+                `nilai v yang diprediksi adalah ${jres[0]} volt`
             );   
             bot.sendMessage(
                 msg.chat.id,
-                `nilai p yang diprediksi adalah s.{jres[1]} watt`
+                `nilai p yang diprediksi adalah ${jres[1]} watt`
             );   
         })
     }else{
